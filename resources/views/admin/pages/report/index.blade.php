0306c12ce30c9d386f8b628a255c585a35819091
muji101 <mujikuwat91@gmail.com>
2022-01-11 09:44:06 +0700

    laporan tambahan

--- resources/views/admin/pages/report/index.blade.php
+++ resources/views/admin/pages/report/index.blade.php
@@ -94,8 +94,108 @@
             </div>
         </div>
 
-        <div class="row my-5">
-            <h3>Grafik Pendapatan</h3>
+        <div class="py-5">
+            <h5>Kursus paling banyak diambil</h5>
+            <div class="card p-2">
+                <div class="table-responsive p-0">
+                    <table class="table align-items-center mb-0">
+                        <thead>
+                            <tr>
+                                <th class="text-uppercase text-secondary text-xxs font-weight-bolder ps-2 opacity-7">Kursus</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Kursus Diambil</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Pendapatan</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Instruktur</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div class="d-flex flex-column justify-content-center">
+                                        <h6 class="mb-0 text-sm">Kursus Menggambar</h6>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-center text-xs font-weight-bold mb-0">4</p>
+                                </td>
+                                <td class="align-middle text-center text-sm">
+                                    <span class="text-secondary text-xs font-weight-bold">Rp 1.600.000</span>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <p class="text-xs font-weight-bold mb-0">Joko anwar</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="d-flex flex-column justify-content-center">
+                                        <h6 class="mb-0 text-sm">Kursus Menjahit</h6>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-center text-xs font-weight-bold mb-0">4</p>
+                                </td>
+                                <td class="align-middle text-center text-sm">
+                                    <span class="text-secondary text-xs font-weight-bold">Rp 1.600.000</span>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <p class="text-xs font-weight-bold mb-0">Joko anwar</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="d-flex flex-column justify-content-center">
+                                        <h6 class="mb-0 text-sm">Kursus Menari</h6>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-center text-xs font-weight-bold mb-0">4</p>
+                                </td>
+                                <td class="align-middle text-center text-sm">
+                                    <span class="text-secondary text-xs font-weight-bold">Rp 1.600.000</span>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <p class="text-xs font-weight-bold mb-0">Joko anwar</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="d-flex flex-column justify-content-center">
+                                        <h6 class="mb-0 text-sm">Kursus Bahasa Inggris</h6>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-center text-xs font-weight-bold mb-0">4</p>
+                                </td>
+                                <td class="align-middle text-center text-sm">
+                                    <span class="text-secondary text-xs font-weight-bold">Rp 1.600.000</span>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <p class="text-xs font-weight-bold mb-0">Joko anwar</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="d-flex flex-column justify-content-center">
+                                        <h6 class="mb-0 text-sm">Kursus Bahasa Arab</h6>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p class="text-center text-xs font-weight-bold mb-0">4</p>
+                                </td>
+                                <td class="align-middle text-center text-sm">
+                                    <span class="text-secondary text-xs font-weight-bold">Rp 1.600.000</span>
+                                </td>
+                                <td class="align-middle text-center">
+                                    <p class="text-xs font-weight-bold mb-0">Joko anwar</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <h5>Grafik Pendapatan</h5>
             <div class="col-12">
                 <div class="card bg-gradient-info">
                     <div class="chart">
