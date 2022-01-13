d58e71923333673d9efbbe7918ed510762677908
gomgomgit <thechocohead@gmail.com>
2022-01-13 21:25:53 +0700

    dashboard admin

--- resources/views/admin/index_dashboard.blade.php
+++ resources/views/admin/index_dashboard.blade.php
@@ -14,7 +14,7 @@
                 <div class="card-body">
                 <div class="text-center">
                     <i class="material-icons opacity-10 text-light p-3 bg-blue rounded-circle fs-1">menu_book</i>
-                    <h4>Endrolled Courses</h4>
+                    <h4>Enrolled Courses</h4>
                     <h1>12</h1>
                 </div>
                 </div>
@@ -319,70 +319,62 @@
                 </div>
             </div>
         </div>
+        
         <div class="col-lg-4 col-md-6">
             <div class="card h-100">
                 <div class="card-header pb-0">
-                    <h6>Orders overview</h6>
-                    <p class="text-sm">
-                        <i class="fa fa-arrow-up text-success" aria-hidden="true"></i>
-                        <span class="font-weight-bold">24%</span> this month
-                    </p>
+                    <h6>Pembelian terakhir</h6>
                 </div>
                 <div class="card-body p-3">
-                    <div class="timeline timeline-one-side">
-                        <div class="timeline-block mb-3">
-                            <span class="timeline-step">
-                                <i class="material-icons text-success text-radial">notifications</i>
-                            </span>
-                            <div class="timeline-content">
-                                <h6 class="text-dark text-sm font-weight-bold mb-0">$2400, Design changes</h6>
-                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
-                            </div>
+                    
+                    <div class="my-3 mx-2 d-flex justify-content-between">
+                        <div>
+                            <h6 class="text-dark text-sm font-weight-bold mb-0">Ichsan Hasibuan</h6>
+                            <h6 class="text-dark text-sm fw-normal mb-0">Kursus Design changes</h6>
                         </div>
-                        <div class="timeline-block mb-3">
-                            <span class="timeline-step">
-                                <i class="material-icons text-danger text-radial">code</i>
-                            </span>
-                            <div class="timeline-content">
-                                <h6 class="text-dark text-sm font-weight-bold mb-0">New order #1832412</h6>
-                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 11 PM</p>
-                            </div>
+                        <div class="">
+                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">Rp 300.000</p>
+                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
                         </div>
-                        <div class="timeline-block mb-3">
-                            <span class="timeline-step">
-                                <i class="material-icons text-dark text-radial">shopping_cart</i>
-                            </span>
-                            <div class="timeline-content">
-                                <h6 class="text-dark text-sm font-weight-bold mb-0">Server payments for April</h6>
-                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 9:34 PM</p>
-                            </div>
+                    </div>
+                    <div class="my-3 mx-2 d-flex justify-content-between">
+                        <div>
+                            <h6 class="text-dark text-sm font-weight-bold mb-0">Ichsan Hasibuan</h6>
+                            <h6 class="text-dark text-sm fw-normal mb-0">Kursus Design changes</h6>
                         </div>
-                        <div class="timeline-block mb-3">
-                            <span class="timeline-step">
-                                <i class="material-icons text-warning text-radial">credit_card</i>
-                            </span>
-                            <div class="timeline-content">
-                                <h6 class="text-dark text-sm font-weight-bold mb-0">New card added for order #4395133</h6>
-                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">20 DEC 2:20 AM</p>
-                            </div>
+                        <div class="">
+                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">Rp 300.000</p>
+                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
                         </div>
-                        <div class="timeline-block mb-3">
-                            <span class="timeline-step">
-                                <i class="material-icons text-primary text-radial">key</i>
-                            </span>
-                            <div class="timeline-content">
-                                <h6 class="text-dark text-sm font-weight-bold mb-0">Unlock packages for development</h6>
-                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">18 DEC 4:54 AM</p>
-                            </div>
+                    </div>
+                    <div class="my-3 mx-2 d-flex justify-content-between">
+                        <div>
+                            <h6 class="text-dark text-sm font-weight-bold mb-0">Ichsan Hasibuan</h6>
+                            <h6 class="text-dark text-sm fw-normal mb-0">Kursus Design changes</h6>
                         </div>
-                        <div class="timeline-block">
-                            <span class="timeline-step">
-                                <i class="material-icons text-dark text-radial">payments</i>
-                            </span>
-                            <div class="timeline-content">
-                                <h6 class="text-dark text-sm font-weight-bold mb-0">New order #9583120</h6>
-                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">17 DEC</p>
-                            </div>
+                        <div class="">
+                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">Rp 300.000</p>
+                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
+                        </div>
+                    </div>
+                    <div class="my-3 mx-2 d-flex justify-content-between">
+                        <div>
+                            <h6 class="text-dark text-sm font-weight-bold mb-0">Ichsan Hasibuan</h6>
+                            <h6 class="text-dark text-sm fw-normal mb-0">Kursus Design changes</h6>
+                        </div>
+                        <div class="">
+                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">Rp 300.000</p>
+                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
+                        </div>
+                    </div>
+                    <div class="my-3 mx-2 d-flex justify-content-between">
+                        <div>
+                            <h6 class="text-dark text-sm font-weight-bold mb-0">Ichsan Hasibuan</h6>
+                            <h6 class="text-dark text-sm fw-normal mb-0">Kursus Design changes</h6>
+                        </div>
+                        <div class="">
+                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">Rp 300.000</p>
+                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
                         </div>
                     </div>
                 </div>
